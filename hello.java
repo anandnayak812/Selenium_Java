b7a0d8270593e7c79eda887e27947de8d0addac5
Anand Nayak <anandnayak812@gmail.com>
2023-07-29 20:21:45 +0530

    HTML Code

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello{
+public static void main(String[] args) {
+    System.out.println("Hello World");
+    love lo = new love();
+}
+}
+public String love(){
+    String value = "First one";
+return value;
+}
+
+public String love(String value1){
+    String value = "Second one";
+return value;
+}
